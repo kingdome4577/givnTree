5c444b062d89165d34809d253c7b6d08afcbd1a4
Arman Gurkan <arman@secelektrik.com>
2020-02-19 20:05:12 -0800

    created an initial model to further work on

--- server/controllers/volunController.js
+++ server/controllers/volunController.js
@@ -1,1 +1,14 @@
-const router = require('express')
+const db = require('../models/starWarsModels');
+
+const volunController = {};
+
+volunController.createVolun = (req, res, next) => {
+}
+volunController.updateVolun = (req, res, next) => {
+}
+volunController.deleteVolun = (req, res, next) => {
+}
+volunController.getAListofVoluns = (req, res, next) => {
+}
+
+module.exports = volunController;
